adc175da647c15c32f996ead1b08093aecc6b5d3
Angel <angel_epa_luses@hotmail.com>
2019-08-07 19:04:24 -0500

    0.6

--- script/navBar.js
+++ script/navBar.js
@@ -1,6 +1,7 @@
 var index_1 = false;
 var index_2 = false;
 
+
 const content = (x) => {
     x.classList.toggle("change");
  
@@ -9,10 +10,13 @@ active_layer()
 }
 
 const active_layer = () => {
-    let content_box_1 = document.getElementsByClassName('bar-content')
+    let content_box_1 = document.getElementsByClassName
+    ('bar-content')
+    let layer1 = document.getElementById("layer_1")
     if (index_1 === false) {
         index_1 = true
         content_box_1[0].style.width = "100%"
+        layer1.style.width = "100%"
 console.log(index_1)
     }
     else if (index_1 === true) {
@@ -24,4 +28,17 @@ console.log(index_1)
 
 const portals = [""]
 
-const portal_1 = "<ul class='grid-container' ><li><a href='#'>RESEARCH</a></li><li><a href='#'>RESEARCH</a></li><li><a href='#'>RESEARCH</a></li><li><a href='#'>RESEARCH</a></li><li><a href='#'>RESEARCH</a></li></ul>"
+const portal_1 = ""
+
+
+var slide_index;
+const grid_slide = (e) => {
+    let layer1 = document.getElementById("layer_1")
+var slides = document.getElementsByClassName("grid_slider")
+for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+}
+slides[e-1].style.display = "block"
+layer1.style.width = "0"
+}
+grid_slide(slide_index)
